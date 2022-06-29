e2c7ced5e3b9a32ca00209115b4b833ae8149db6
Mike <ekimmai@outlook.com>
2022-06-30 00:30:59 +0300

    added functionality to drop down menu related to items in list

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 
 //event listeners
 
@@ -9,6 +10,8 @@ const todoList = document.querySelector('.todo-list');
 todoButton.addEventListener('click', addTodo);
 //adding an event listener to delete items? change so it MOVES ITEMS TO TRASH HTML page
 todoList.addEventListener('click', deleteCheck);
+//event listener for drop-down menu
+filterOption.addEventListener('click', filterTodo);
 
 //functions
 
@@ -60,4 +63,29 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+            break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+            break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+            break;
+        }
+    });
 }
