93e1f44392d2b705ae04dd7f79b9e0af68b7e813
Mike <ekimmai@outlook.com>
2022-06-29 21:36:11 +0300

    added function to add elements to homepage with buttons

--- todo.js
+++ todo.js
@@ -1,1 +1,37 @@
-//javascript for todo app
+//selectors for getting elements
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+
+//add listener to add button and calls on function to create new element
+todoButton.addEventListener('click', addTodo);
+
+//functions
+
+//todo list item with buttons at the end
+function addTodo(event) {
+    //prevent form from submitting
+    event.preventDefault();
+    //todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+}
