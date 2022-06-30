3ef98ab330b8c67f087d2a1b94028d65ab935750
Mike <ekimmai@outlook.com>
2022-06-30 21:37:36 +0300

    updated express routes and installed ejs package

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
-
-//express app
 const app = express();
+//register view engine
+app.set('view engine', 'ejs'); 
 
-//listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  
-  //res.send();
   res.sendFile('/index.html', { root: __dirname});
+});
 
+app.get('/index.html', (req, res) => {
+  res.sendFile('/index.html', { root: __dirname});
 });
 
 app.get('/trash.html', (req, res) => {
-  
-  //res.send();
   res.sendFile('/trash.html', { root: __dirname});
-
 });
+//404 page if request not found
+app.use((req, res) =>{
+  res.status(404).sendFile('/404.html', {root: __dirname})
+})
