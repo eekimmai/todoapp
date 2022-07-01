6841d3040232c0e2fa5dabc9525d87d2a4b54277
Mike <ekimmai@outlook.com>
2022-07-01 03:44:51 +0300

    trying to add connectivity between index button of delete and mongodb

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const todoitems = require('./models/todolist');
 //connect to mongodb
 const dbURI = 'mongodb+srv://newuser:test1234@cluster0.3l0fxpt.mongodb.net/todoapplist?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,9 +14,28 @@ app.set('view engine', 'ejs');
 //middleware for static files
 app.use(express.static('public'));
 
+//routes
+
 app.get('/', (req, res) => {
   res.render('index');
-});
+})
+//push new item to mongo db upon get request
+/*
+app.get('/trash', (req, res) => {
+  const item = new todoitems({
+  //find a way to use this line to get items from todo list once delete button is clicked
+  text: 'new item2',
+  }); 
+
+  item.save()
+    .then((result) => {
+      //res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+*/
 
 app.get('/index.html', (req, res) => {
   res.render('index');
